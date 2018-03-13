7e81b15d0ceea873a7336f6321043670e825aa56
miyaluc <srlucas3@gmail.com>
2018-03-13 16:12:43 -0700

    created compress method

---
+++ Compress_a_String/Compress_a_String/Compression_Factory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compress_a_String
+{
+    class Compression_Factory
+    {
+        //Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabccccaaa would become a2b1c4a3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters.
+
+        public string Compress(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            int countConsecutive = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                countConsecutive++;
+
+                if (i + 1 >= input.Length || input[i] != input[i + 1])
+                {
+                    sb.Append(input[i]);
+                    sb.Append(countConsecutive);
+                    countConsecutive = 0;
+                }
+            }
+            return sb.Length < input.Length ? sb.ToString() : input;
+        }
+    }
+}
---
+++ Compress_a_String/Compress_a_String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compress_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
