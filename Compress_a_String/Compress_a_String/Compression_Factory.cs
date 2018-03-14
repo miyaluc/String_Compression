9c9a8cfa79b882855b58c5aca176bd1fa105bcc2
miyaluc <srlucas3@gmail.com>
2018-03-14 11:14:56 -0700

    added sudo code to solution

--- Compress_a_String/Compress_a_String/Compression_Factory.cs
+++ Compress_a_String/Compress_a_String/Compression_Factory.cs
@@ -10,21 +10,29 @@ namespace Compress_a_String
 
         public string Compress(string input)
         {
+            //StringBuilder creates a resizable array of all strings, copying them back to a string only when necessary
             StringBuilder sb = new StringBuilder();
             int countConsecutive = 0;
 
             for (int i = 0; i < input.Length; i++)
             {
+                //this keeps track of how many times a letter is displayed
                 countConsecutive++;
 
+                //if the next value is equal to null OR if the next character is different than the current
                 if (i + 1 >= input.Length || input[i] != input[i + 1])
                 {
+                    //append input at i to string sb
                     sb.Append(input[i]);
+                    //append the number of times a particular letter was seen
                     sb.Append(countConsecutive);
+                    //set count to 0 for new letter in string input
                     countConsecutive = 0;
                 }
             }
+            //output compressed string sb
             Console.WriteLine(sb);
+            //if length of sb is less than length of input, return sb -- ELSE return original input
             return sb.Length < input.Length ? sb.ToString() : input;
         }
     }
